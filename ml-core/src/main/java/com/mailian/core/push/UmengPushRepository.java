416e666de244fa0f389bceb863eb71ab639f3240
wangqiaoqing <wangqq@szmlink.com>
2018-11-28 15:01:48 +0800

    消息推送

--- ml-core/src/main/java/com/mailian/core/push/UmengPushRepository.java
+++ ml-core/src/main/java/com/mailian/core/push/UmengPushRepository.java
@@ -10,14 +10,34 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class UmengPushRepository {
-    @Value("${umengpush.appkey}")
-    private String APP_KEY;
-    @Value("${umengpush.appMasterSecret}")
-    private String APP_MASTERSECRET;
+    @Value("${manager.umengpush.appkey}")
+    private String MANAGER_APP_KEY;
+    @Value("${manager.umengpush.appMasterSecret}")
+    private String MANAGER_APP_MASTERSECRET;
+
+    @Value("${user.umengpush.appkey}")
+    private String USER_APP_KEY;
+    @Value("${user.umengpush.appMasterSecret}")
+    private String USER_APP_MASTERSECRET;
     private static final String ALIAS_TYPE = "uniqueKey";
 
-    public void sendAndroidCustomizedcast(String uniqueKey,String title,Integer builderId,String text) throws Exception {
-        AndroidCustomizedcast customizedcast = new AndroidCustomizedcast(APP_KEY,APP_MASTERSECRET);
+    public void sendManagerAndroidCustomizedcast(String uniqueKey,String title,Integer builderId,String text) throws Exception {
+        AndroidCustomizedcast customizedcast = new AndroidCustomizedcast(MANAGER_APP_KEY,MANAGER_APP_MASTERSECRET);
+        customizedcast.setAlias(uniqueKey, ALIAS_TYPE);
+        customizedcast.setTicker( "Android customizedcast ticker");
+        customizedcast.setTitle(title);
+        customizedcast.setBuilderId(builderId+10);
+        customizedcast.setText(text);
+        customizedcast.goAppAfterOpen();
+        customizedcast.setDisplayType(AndroidNotification.DisplayType.NOTIFICATION);
+        customizedcast.setProductionMode();
+
+        PushClient client = new PushClient();
+        client.send(customizedcast);
+    }
+
+    public void sendUserAndroidCustomizedcast(String uniqueKey,String title,Integer builderId,String text) throws Exception {
+        AndroidCustomizedcast customizedcast = new AndroidCustomizedcast(USER_APP_KEY,USER_APP_MASTERSECRET);
         customizedcast.setAlias(uniqueKey, ALIAS_TYPE);
         customizedcast.setTicker( "Android customizedcast ticker");
         customizedcast.setTitle(title);
